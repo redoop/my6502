ae8a6dcd171f9a8b667d626a95752199e2dbf34e
tongxiaojun <tongxiaojun@redoop.com>
2025-11-30 12:14:58 +0800

    Remove obj_dir_module generated files

--- src/test/rtl/obj_dir_module/nes_module_main.d
+++
@@ -1,6 +0,0 @@
-nes_module_main.o: ../nes_module_main.cpp Vnes_module_test.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated_funcs.h
