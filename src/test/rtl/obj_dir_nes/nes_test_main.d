0f659c6be06361260e40468a2c72e0bedd44ff2b
tongxiaojun <tongxiaojun@redoop.com>
2025-11-30 12:13:09 +0800

    Remove generated Verilator files from version control

--- src/test/rtl/obj_dir_nes/nes_test_main.d
+++
@@ -1,6 +0,0 @@
-nes_test_main.o: ../nes_test_main.cpp Vnes_unit_test.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.042/share/verilator/include/verilated_funcs.h
